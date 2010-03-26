7a73816a06ba2c1c49ccc175d52bef5ba6e55acc
Roman Kharitonov <refaim.vl@gmail.com>
2010-03-26 12:41:08 +1000

    Fill screen function

--- graphic.asm
+++ graphic.asm
@@ -3,7 +3,7 @@
 locals
 
 public get_display_mode, set_display_mode
-public get_point_offset_13h
+public get_point_offset_13h, fill_screen_13h
 
 code segment para public 'code' use16
 assume cs: code
@@ -23,6 +23,10 @@ uses ax
     ret
 set_display_mode endp
 
+
+; ATTENTION
+; for next functions es must contain video memory offset
+
 ; out: si -- offset of point (x, y) in video memory (display mode 13h)
 get_point_offset_13h proc pascal far x: word, y: word
 uses ax
@@ -32,6 +36,19 @@ uses ax
     mov si, ax
     ret
 get_point_offset_13h endp
+
+fill_screen_13h proc pascal far color: byte
+uses ax, si
+    mov al, color
+    mov si, 0FFFFh
+@@draw:
+    mov es:[si], al
+    dec si
+    test si, si
+    jnz short @@draw
+    mov es:0, al
+    ret
+fill_screen_13h endp
 
 code ends
 end
